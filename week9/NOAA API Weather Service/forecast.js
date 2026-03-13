18c543405d19110606bd2afe25ef19bf44771f9a
haydenmk <haydenmk@hawaii.edu>
2026-03-13 05:38:01 -1000

    Update forecast fetching to use hourly forecast

--- week9/NOAA API Weather Service/forecast.js
+++ week9/NOAA API Weather Service/forecast.js
@@ -1,12 +1,12 @@
 async function getForecast() {
 
-    const point = await fetch("https://api.weather.gov/points/20.8893,-156.4729");
-    const pointData = await point.json();
+    const point = await fetch("https://api.weather.gov/points/20.8893,-156.4729"); // Gathers data from weather.gov's API with latitude and longitude for Kahului
+    const pointData = await point.json(); // Converts the response to JSON format
 
-    const forecast = await fetch(pointData.properties.forecastHourly);
-    const forecastData = await forecast.json();
+    const forecast = await fetch(pointData.properties.forecast); // Fetches the forecast data from the URL provided previously
+    const forecastData = await forecast.json(); // Converts the response to JSON format
 
-    console.log(forecastData.properties.periods[0].detailedForecast);
+    console.log(forecastData.properties.periods[0].detailedForecast); // Logs the detailed forecast in the console
 }
 
 getForecast();
